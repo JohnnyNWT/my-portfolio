f574c101ea0c9523a0e9f061cc8110b2ac8c9e82
JohnnyNWT <jooaaooskt@gmail.com>
2023-02-17 15:04:53 -0300

    FEAT: cria esqueleto da page About

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import About from './pages/About';
 import Home from './pages/Home';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
         <div>
           <Header />
           <Home />
+          <About />
         </div>
     );
   }
