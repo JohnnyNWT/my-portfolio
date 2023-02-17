1b4f58fd7fdb4e6409ff1a1274143d6b3e01c0d9
JohnnyNWT <jooaaooskt@gmail.com>
2023-02-17 01:32:22 -0300

    FEAT: cria component header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        <a>HOME</a>
+        <a>SOBRE MIM</a>
+        <a>PROJETOS</a>
+        <a>SKILLS</a>
+        <a>CONTATO</a>
+      </div>
+    );
+  }
+}
+
+export default Header;
