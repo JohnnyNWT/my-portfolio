794b4c3c700836737f2719bc0aa137d409926557
JohnnyNWT <jooaaooskt@gmail.com>
2023-10-24 11:08:20 -0300

    FEAT: add importações de imagens no data.projects

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,39 +1,43 @@
+import RecipesAPP from '../images/recipes-app-icon.png';
+import TrybeTunes from '../images/trybetunes-icon.png';
+import EmBreve from '../images/em-breve-icon.png';
+
 const dataProjects = [{
-  srcImage: "https://a.imagem.app/ozaYPy.png",
+  srcImage: TrybeTunes,
   projectName: "TrybeTunes",
   projectDescription: "O Trybe Tunes é uma aplicação capaz de reproduzir músicas, criar uma lista de músicas favoritas e editar o perfil da pessoa usuária logada",
   linkGitHubProject: "https://github.com/JohnnyNWT/trybe-tunes",
   linkViewProject: "https://project-trybetunes-lovat.vercel.app/",
 }, {
-  srcImage: "https://a.imagem.app/oYd3QW.png",
+  srcImage: RecipesAPP,
   projectName: "Recipes App",
   projectDescription: "O Recipes App é uma plataforma culinária completa que permite aos usuários explorar uma vasta coleção de receitas e drinks, personalizar seus perfis, e salvar suas receitas favoritas em um só lugar.",
   linkGitHubProject: "https://github.com/JohnnyNWT/recipes-app",
   linkViewProject: "https://recipes-app-rouge-two.vercel.app/",
 }, {
-  srcImage: "https://a.imagem.app/o7Shgb.png",
+  srcImage: EmBreve,
   projectName: "EM BREVE",
-  projectDescription: "LOREM IPSUM",
+  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
 }, {
-  srcImage: "https://a.imagem.app/o7Shgb.png",
+  srcImage: EmBreve,
   projectName: "EM BREVE",
-  projectDescription: "LOREM IPSUM",
+  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
 }, {
-  srcImage: "https://a.imagem.app/o7Shgb.png",
+  srcImage: EmBreve,
   projectName: "EM BREVE",
-  projectDescription: "LOREM IPSUM",
+  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
 }, {
-  srcImage: "https://a.imagem.app/o7Shgb.png",
+  srcImage: EmBreve,
   projectName: "EM BREVE",
-  projectDescription: "LOREM IPSUM",
+  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
 }, {
-  srcImage: "https://a.imagem.app/o7Shgb.png",
+  srcImage: EmBreve,
   projectName: "EM BREVE",
-  projectDescription: "LOREM IPSUM",
+  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
 }, {
-  srcImage: "https://a.imagem.app/o7Shgb.png",
+  srcImage: EmBreve,
   projectName: "EM BREVE",
-  projectDescription: "LOREM IPSUM",
+  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
 }];
 
 export default dataProjects;
