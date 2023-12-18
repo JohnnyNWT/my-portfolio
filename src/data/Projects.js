50382eb319d7c8165f90e003b1eafe185883fd58
JohnnyNWT <jooaaooskt@gmail.com>
2023-12-18 16:08:45 -0300

    FEAT: responsive Projects page

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -22,22 +22,6 @@ const dataProjects = [{
   srcImage: EmBreve,
   projectName: "EM BREVE",
   projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-}, {
-  srcImage: EmBreve,
-  projectName: "EM BREVE",
-  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-}, {
-  srcImage: EmBreve,
-  projectName: "EM BREVE",
-  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-}, {
-  srcImage: EmBreve,
-  projectName: "EM BREVE",
-  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-}, {
-  srcImage: EmBreve,
-  projectName: "EM BREVE",
-  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
 }];
 
 export default dataProjects;
