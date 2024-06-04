adf27593143ec3b918cd0f685d015889f857cad1
JohnnyNWT <jooaaooskt@gmail.com>
2024-06-03 21:43:28 -0400

    FEAT: atualizando home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import minhaImgIA from '../images/joao-marcelo-IA.png';
+import { TypeAnimation } from 'react-type-animation';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "../css/Home.css"
 
@@ -13,7 +14,7 @@ class Home extends React.Component {
   componentDidMount() {
     const intersectionObserver = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
           entry.target.classList.add('show');
         } else {
           entry.target.classList.remove('show');
@@ -31,15 +32,52 @@ class Home extends React.Component {
   }
 
   render() {
-    return(
-      <div id='Home' className="container-home">
-        <img src={ minhaImgIA } alt="Joao Marcelo IA" className="img-home hidden" ref={this.homeImgRef}/>
-        <div className="container-info hidden" ref={this.homeDivRef}>
-          <h1 className="text-home">Olá, eu sou o</h1>
-          <h1 className="text-home">João Marcelo</h1>
-          <h2 className="text-home">Desenvolvedor web full-stack</h2>
+    return (
+      <div id="Home" className="container-home">
+        <img src={minhaImgIA} alt="Joao Marcelo IA" className="img-home hidden" ref={this.homeImgRef} />
+        <div className="home-info hidden" ref={this.homeDivRef}>
+          <h3 className="text-home">Olá, Me Chamo</h3>
+          <h1 className="text-home2">João Marcelo</h1>
+          <TypeAnimation
+            sequence={[
+              `Desenvolvedor de Software!`,
+              1500,
+              'Designer!',
+              1500,
+              'Freelancer!',
+              1500,
+              'Analista de Dados!',
+              1500
+            ]}
+            className="highlights"
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
+          <p className="paragraph">
+            Apaixonado por desafios, estou em constante busca pela excelência no desenvolvimento de software.
+            Sou comprometido em criar soluções eficazes e inovadoras.
+            Trabalho muito bem em equipe e sou orientado para gerar resultados.
+          </p>
+
+          <div className="container-social-icons">
+            <a href="https://github.com/JohnnyNWT" target="_blank" className="icon-home">
+              <i className="bi bi-github"></i>
+            </a>
+            <a href="mailto:jooaaooskt@gmail.com" target="_blank" className="icon-home">
+              <i className="bi bi-envelope"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/johnnydev/" target="_blank" className="icon-home">
+              <i className="bi bi-linkedin"></i>
+            </a>
+          </div>
         </div>
-      </div>
+        <a href="#Home">
+          <div className="iconTop">
+            <i class="bi bi-arrow-up"></i>
+          </div>
+        </a>
+        </div>
     );
   }
 }
