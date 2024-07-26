d24323ecdb5a2710304d1e5b052e153b5fe67f64
JohnnyNWT <jooaaooskt@gmail.com>
2024-07-26 01:50:11 -0300

    FEAT: add novo projeto

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -2,8 +2,20 @@ import RecipesAPP from '../images/recipes-app-icon.png';
 import TrybeTunes from '../images/trybetunes-icon.png';
 import EmBreve from '../images/em-breve-icon.png';
 import MenuOnline from '../images/menu-online-icon.png';
+import LeoPremios from '../images/leo-premios-icon.png';
 
 const dataProjects = [{
+  srcImage: MenuOnline,
+  projectName: "Menu Online",
+  projectDescription: "O Menu Online é uma solução moderna e eficiente para transformar a experiência de pedidos em restaurantes e estabelecimentos de comida. Com o Menu Online, os clientes podem explorar facilmente o cardápio, selecionar os itens desejados e adicioná-los ao carrinho de compras de forma intuitiva",
+  linkViewProject: "https://menu-online-zeta.vercel.app/",
+}, {
+  srcImage: LeoPremios,
+  projectName: "Leo Prêmios",
+  projectDescription: "O Leo Premios é uma plataforma inovadora para rifas digitais, onde os usuários podem visualizar rifas passadas, conhecer os ganhadores, acessar a rifa atual e esclarecer suas dúvidas de forma prática e eficiente.",
+  linkGitHubProject: "https://github.com/JohnnyNWT/leo-premios",
+  linkViewProject: "https://xn--leopremiaoes-sdb.com/",
+}, {
   srcImage: TrybeTunes,
   projectName: "TrybeTunes",
   projectDescription: "O Trybe Tunes é uma aplicação capaz de reproduzir músicas, criar uma lista de músicas favoritas e editar o perfil da pessoa usuária logada",
@@ -15,15 +27,6 @@ const dataProjects = [{
   projectDescription: "O Recipes App é uma plataforma culinária completa que permite aos usuários explorar uma vasta coleção de receitas e drinks, personalizar seus perfis, e salvar suas receitas favoritas em um só lugar",
   linkGitHubProject: "https://github.com/JohnnyNWT/recipes-app",
   linkViewProject: "https://recipes-app-rouge-two.vercel.app/",
-}, {
-  srcImage: MenuOnline,
-  projectName: "Menu Online",
-  projectDescription: "O Menu Online é uma solução moderna e eficiente para transformar a experiência de pedidos em restaurantes e estabelecimentos de comida. Com o Menu Online, os clientes podem explorar facilmente o cardápio, selecionar os itens desejados e adicioná-los ao carrinho de compras de forma intuitiva",
-  linkViewProject: "https://menu-online-zeta.vercel.app/",
-}, {
-  srcImage: EmBreve,
-  projectName: "EM BREVE",
-  projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
 }];
 
 export default dataProjects;
